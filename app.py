89d0ab67c0c50b354decacafcc8e1aa85d4e3f20
Robert Starmer <rstarmer@gmail.com>
2020-03-25 08:07:46 +0000

    Added bigger buttons. color for reps, forward/back

--- app.py
+++ app.py
@@ -20,8 +20,9 @@ See "start.sh" for running the app, or better yet, build the container and
 run it locally.
 '''
 
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, redirect, url_for, render_template
 import random
+from datetime import datetime
 import os
 import sys
 
@@ -29,8 +30,12 @@ app = Flask(__name__)
 
 default_reps=["1","2","3","4"]
 
-
 @app.route('/')
+def index():
+    seed=datetime.strftime(datetime.now().date(),'%m%d%y')
+    return redirect(url_for('draw',seed=seed,set='1'))
+
+@app.route('/go')
 def draw():
     cards=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]
     suits=[]
@@ -42,17 +47,14 @@ def draw():
     suits.append(request.args.get("hearts","crunches"))
     seed=request.args.get("seed","")
     rep_set.append(request.args.get("set","1,2,3,4"))
-    print("pre rep_set validation: {}".format(rep_set))
 
     if not rep_set[0] in default_reps:
-        print("bad rep_set")
-        rep_set=[1,2,3,4]
+        rep_set=[1]
         next_set=None
     else:
-        print("good rep_set")
         rep_set=[int(rep_set[0])]
         if rep_set[0] < 4:
-            next_set="/?set={}&diamonds={}&spades={}&clubs={}&hearts={}&seed={}".format(
+            next_set="/go?set={}&diamonds={}&spades={}&clubs={}&hearts={}&seed={}".format(
                 rep_set[0]+1,
                 suits[0],
                 suits[1],
